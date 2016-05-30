dd3d12f5e7c5c588cc17a8def76899bfa000d822
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-05-30 16:06:30 +0200

    Add naive implementation of `[]` and `push_back`
    This implementation follow very closely the Clojure one.  The
    implementation is *naive*, in the sense that it discards optimization
    opportunities in favor of debuggability and correctness.  We will
    optimize later.

--- test/vektor.cpp
+++ test/vektor.cpp
@@ -7,5 +7,29 @@ using namespace immu;
 TEST_CASE("instantiation")
 {
     auto v = vektor<int>{};
-    CHECK(v.size() == 0);
+    CHECK(v.size() == 0u);
+}
+
+TEST_CASE("push back")
+{
+    SUBCASE("one element")
+    {
+        const auto v1 = vektor<int>{};
+        auto v2 = v1.push_back(42);
+        CHECK(v1.size() == 0u);
+        CHECK(v2.size() == 1u);
+        CHECK(v2[0] == 42);
+    }
+
+    SUBCASE("many elements")
+    {
+        const auto n = 666u;
+        auto v = vektor<unsigned>{};
+        for (auto i = 0u; i < n; ++i) {
+            v = v.push_back(i * 42);
+            CHECK(v.size() == i + 1);
+            for (auto j = 0u; j < v.size(); ++j)
+                CHECK(v[j] == j * 42);
+        }
+    }
 }
