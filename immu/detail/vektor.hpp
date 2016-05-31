c4bd00214a335884933be2859525b340ba73e684
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-05-31 17:33:28 +0200

    Extract data structure implementation into detail namespace
    This separates the STL-like interface from the algorithms, etc.  This
    makes will make it easier to separate the concern of building a nice
    C++-sy API and implementing the algorithms.  Specially, when transients
    and so on are added to the equation.

---
+++ immu/detail/vektor.hpp
@@ -0,0 +1,256 @@
+
+#pragma once
+
+#include <eggs/variant.hpp>
+#include <boost/iterator/iterator_facade.hpp>
+#include <memory>
+
+namespace immu {
+namespace detail {
+namespace vektor {
+
+constexpr auto branching_log  = 5;
+constexpr auto branching      = 1 << branching_log;
+constexpr auto branching_mask = branching - 1;
+
+template <typename T>
+struct node;
+
+template <typename T>
+using node_ptr = std::shared_ptr<node<T> >;
+
+template <typename T>
+using leaf_node  = std::array<T, branching>;
+
+template <typename T>
+using inner_node = std::array<node_ptr<T>, branching>;
+
+using eggs::variants::get;
+
+template <typename T>
+struct node : eggs::variant<leaf_node<T>, inner_node<T>>
+{
+    using base_t = eggs::variant<leaf_node<T>, inner_node<T>>;
+    using base_t::base_t;
+
+    inner_node<T>& inner() & { return get<inner_node<T>>(*this); }
+    const inner_node<T>& inner() const& { return get<inner_node<T>>(*this); }
+    inner_node<T>&& inner() && { return get<inner_node<T>>(std::move(*this)); }
+
+    leaf_node<T>& leaf() & { return get<leaf_node<T>>(*this); }
+    const leaf_node<T>& leaf() const& { return get<leaf_node<T>>(*this); }
+    leaf_node<T>&& leaf() && { return get<leaf_node<T>>(std::move(*this)); }
+};
+
+template <typename T, typename ...Ts>
+auto make_node(Ts&& ...xs)
+{
+    return std::make_shared<node<T>>(std::forward<Ts>(xs)...);
+}
+
+template <typename T>
+struct impl
+{
+    using inner_t    = inner_node<T>;
+    using leaf_t     = leaf_node<T>;
+    using node_t     = node<T>;
+    using node_ptr_t = node_ptr<T>;
+
+    std::size_t size;
+    unsigned    shift;
+    node_ptr_t  root;
+    node_ptr_t  tail;
+
+    auto tail_offset() const
+    {
+        return size < 32 ? 0 : ((size - 1) >> branching_log) << branching_log;
+    }
+
+    template <typename ...Ts>
+    static auto make_node(Ts&& ...xs)
+    {
+        return vektor::make_node<T>(std::forward<Ts>(xs)...);
+    }
+
+    const leaf_t& array_for(std::size_t index) const
+    {
+        assert(index < size);
+
+        if (index >= tail_offset())
+            return tail->leaf();
+        else {
+            auto node = root.get();
+            for (auto level = shift; level; level -= branching_log) {
+                node = node->inner() [(index >> level) & branching_mask].get();
+            }
+            return node->leaf();
+        }
+    }
+
+    node_ptr_t make_path(unsigned level, node_ptr_t node) const
+    {
+        return level == 0
+            ? node
+            : make_node(inner_t{{ make_path(level - branching_log,
+                                            std::move(node)) }});
+    }
+
+    node_ptr_t push_tail(unsigned level,
+                         const node_t& parent_,
+                         node_ptr_t tail) const
+    {
+        const auto& parent   = parent_.inner();
+        auto new_parent_node = make_node(parent);
+        auto& new_parent     = new_parent_node->inner();
+        auto idx             = ((size - 1) >> level) & branching_mask;
+        auto next_node =
+            level == branching_log ? std::move(tail) :
+            parent[idx]            ? push_tail(level - branching_log,
+                                               *parent[idx],
+                                               std::move(tail)) :
+            /* otherwise */          make_path(level - branching_log,
+                                               std::move(tail));
+        new_parent[idx] = next_node;
+        return new_parent_node;
+    }
+
+    impl push_back(T value) const
+    {
+        auto tail_size = size - tail_offset();
+        if (tail_size < branching) {
+            const auto& old_tail = tail->leaf();
+            auto  new_tail_node  = make_node(leaf_t{});
+            auto& new_tail       = new_tail_node->leaf();
+            std::copy(old_tail.begin(),
+                      old_tail.begin() + tail_size,
+                      new_tail.begin());
+            new_tail[tail_size] = std::move(value);
+            return impl{ size + 1,
+                    shift,
+                    root,
+                    std::move(new_tail_node) };
+        } else {
+            auto new_tail_node = make_node(leaf_t {{ std::move(value) }});
+            return ((size >> branching_log) > (1u << shift))
+                ? impl{ size + 1,
+                    shift + branching_log,
+                    make_node(inner_t{{ root, make_path(shift, tail) }}),
+                    new_tail_node }
+            : impl{ size + 1,
+                      shift,
+                      push_tail(shift, *root, tail),
+                      new_tail_node };
+        }
+    }
+
+    const T& get(std::size_t index) const
+    {
+        const auto& arr = array_for(index);
+        return arr [index & branching_mask];
+    }
+};
+
+template <typename T> const auto    empty_inner = make_node<T>(inner_node<T>{});
+template <typename T> const auto    empty_leaf  = make_node<T>(leaf_node<T>{});
+template <typename T> const impl<T> empty       = {
+    0,
+    branching_log,
+    empty_inner<T>,
+    empty_leaf<T>
+};
+
+template <typename T>
+struct iterator : boost::iterator_facade<
+    iterator<T>,
+    T,
+    boost::random_access_traversal_tag,
+    const T&>
+{
+    struct end_t {};
+
+    iterator() = default;
+
+    iterator(const impl<T>* v)
+        : v_    { v }
+        , i_    { 0 }
+        , base_ { 0 }
+        , curr_ { v->array_for(0).begin() }
+    {
+    }
+
+    iterator(const impl<T>* v, end_t)
+        : v_    { v }
+        , i_    { v->size }
+        , base_ { i_ - (i_ & branching_mask) }
+        , curr_ { v->array_for(i_ - 1).begin() + (i_ - base_) }
+    {}
+
+private:
+    friend class boost::iterator_core_access;
+
+    using leaf_iterator = typename leaf_node<T>::const_iterator;
+
+    const impl<T>* v_;
+    std::size_t    i_;
+    std::size_t    base_;
+    leaf_iterator  curr_;
+
+    void increment()
+    {
+        assert(i_ < v_->size);
+        ++i_;
+        if (i_ - base_ < branching) {
+            ++curr_;
+        } else {
+            base_ += branching;
+            curr_ = v_->array_for(i_).begin();
+        }
+    }
+
+    void decrement()
+    {
+        assert(i_ > 0);
+        --i_;
+        if (i_ >= base_) {
+            --curr_;
+        } else {
+            base_ -= branching;
+            curr_ = std::prev(v_->array_for(i_).end());
+        }
+    }
+
+    void advance(std::ptrdiff_t n)
+    {
+        assert(n <= 0 || i_ + static_cast<std::size_t>(n) <= v_->size);
+        assert(n >= 0 || static_cast<std::size_t>(-n) <= i_);
+
+        i_ += n;
+        if (i_ <= base_ && i_ - base_ < branching) {
+            curr_ += n;
+        } else {
+            base_ = i_ - (i_ & branching_mask);
+            curr_ = v_->array_for(i_).begin() + (i_ - base_);
+        }
+    }
+
+    bool equal(const iterator& other) const
+    {
+        return i_ == other.i_;
+    }
+
+    std::ptrdiff_t distance_to(const iterator& other) const
+    {
+        return other.i_ > i_
+            ?   static_cast<ptrdiff_t>(other.i_ - i_)
+            : - static_cast<ptrdiff_t>(i_ - other.i_);
+    }
+
+    const T& dereference() const
+    {
+        return *curr_;
+    }
+};
+
+} /* namespace vektor */
+} /* namespace detail */
+} /* namespace immu */
--- immu/vektor.hpp
+++ immu/vektor.hpp
@@ -1,287 +1,46 @@
 
 #pragma once
 
-#include <cstdlib>
-#include <memory>
+#include <immu/detail/vektor.hpp>
+
 #include <eggs/variant.hpp>
-#include <boost/iterator/iterator_facade.hpp>
 
 namespace immu {
-
-constexpr auto branching_log  = 5;
-constexpr auto branching      = 1 << branching_log;
-constexpr auto branching_mask = branching - 1;
-
-namespace detail {
-
-template <typename T>
-struct vektor_node;
-template <typename T>
-using vektor_node_ptr = std::shared_ptr<vektor_node<T> >;
-
-template <typename T>
-using vektor_leaf  = std::array<T, branching>;
-template <typename T>
-using vektor_inner = std::array<vektor_node_ptr<T>, branching>;
-
-using eggs::variants::get;
-
-template <typename T>
-struct vektor_node : eggs::variant<vektor_leaf<T>,
-                                   vektor_inner<T>>
-{
-    using base_t = eggs::variant<vektor_leaf<T>,
-                                 vektor_inner<T>>;
-    using base_t::base_t;
-
-    vektor_inner<T>& inner() & { return get<vektor_inner<T>>(*this); }
-    const vektor_inner<T>& inner() const& { return get<vektor_inner<T>>(*this); }
-    vektor_inner<T>&& inner() && { return get<vektor_inner<T>>(std::move(*this)); }
-
-    vektor_leaf<T>& leaf() & { return get<vektor_leaf<T>>(*this); }
-    const vektor_leaf<T>& leaf() const& { return get<vektor_leaf<T>>(*this); }
-    vektor_leaf<T>&& leaf() && { return get<vektor_leaf<T>>(std::move(*this)); }
-};
-
-} // namespace detail
 
 template <typename T>
 class vektor
 {
-    using inner_t  = detail::vektor_inner<T>;
-    using leaf_t   = detail::vektor_leaf<T>;
-    using node_t   = detail::vektor_node<T>;
-    using node_ptr = detail::vektor_node_ptr<T>;
-
-    static const node_ptr empty_leaf_;
-    static const node_ptr empty_inner_;
-    static const vektor   empty_;
-
-    vektor(std::size_t size, unsigned shift, node_ptr root, node_ptr tail)
-        : size_(size)
-        , shift_(shift)
-        , root_(std::move(root))
-        , tail_(std::move(tail))
-    {}
-
-    auto tail_offset_() const
-    {
-        return size_ < 32 ? 0 : ((size_ - 1) >> branching_log) << branching_log;
-    }
-
-    const leaf_t& array_for_(std::size_t index) const
-    {
-        assert(index < size_);
-
-        if (index >= tail_offset_())
-            return tail_->leaf();
-
-        auto node = root_.get();
-        for (auto level = shift_; level; level -= branching_log) {
-            node = node->inner() [(index >> level) & branching_mask].get();
-        }
-        return node->leaf();
-    }
-
-    node_ptr make_path_(unsigned level, node_ptr node) const
-    {
-        return level == 0
-            ? node
-            : make_node_(inner_t{{ make_path_(level - branching_log,
-                                              std::move(node)) }});
-    }
-
-    node_ptr push_tail_(unsigned level,
-                        const node_t& parent_,
-                        node_ptr tail) const
-    {
-        const auto& parent   = parent_.inner();
-        auto new_parent_node = make_node_(parent);
-        auto& new_parent     = new_parent_node->inner();
-        auto idx             = ((size_ - 1) >> level) & branching_mask;
-        auto next_node =
-            level == branching_log ? std::move(tail) :
-            parent[idx]            ? push_tail_(level - branching_log,
-                                                *parent[idx],
-                                                std::move(tail)) :
-            /* otherwise */          make_path_(level - branching_log,
-                                                std::move(tail));
-        new_parent[idx] = next_node;
-        return new_parent_node;
-    }
-
-    template <typename ...Ts>
-    static auto make_node_(Ts&& ...xs)
-    {
-        return std::make_shared<node_t>(std::forward<Ts>(xs)...);
-    }
-
 public:
     using value_type = T;
-    using reference_type = const T&;
+    using reference = const T&;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using const_reference = const T&;
 
-    struct iterator :
-        boost::iterator_facade<iterator,
-                               value_type,
-                               boost::random_access_traversal_tag,
-                               reference_type>
-    {
-        iterator() = default;
-
-    private:
-        friend class vektor;
-        friend class boost::iterator_core_access;
-
-        struct end_t {};
-
-        using leaf_iterator = typename leaf_t::const_iterator;
-        const vektor* v_;
-        std::size_t   i_;
-        std::size_t   base_;
-        leaf_iterator curr_;
-
-        iterator(const vektor* v)
-            : v_    { v }
-            , i_    { 0 }
-            , base_ { 0 }
-            , curr_ { v->array_for_(0).begin() }
-        {
-        }
-
-        iterator(const vektor* v, end_t)
-            : v_    { v }
-            , i_    { v->size_ }
-            , base_ { i_ - (i_ & branching_mask) }
-            , curr_ { v->array_for_(i_ - 1).begin() + (i_ - base_) }
-        {}
-
-        void increment()
-        {
-            assert(i_ < v_->size_);
-            ++i_;
-            if (i_ - base_ < branching) {
-                ++curr_;
-            } else {
-                base_ += branching;
-                curr_ = v_->array_for_(i_).begin();
-            }
-        }
-
-        void decrement()
-        {
-            assert(i_ > 0);
-            --i_;
-            if (i_ >= base_) {
-                --curr_;
-            } else {
-                base_ -= branching;
-                curr_ = std::prev(v_->array_for_(i_).end());
-            }
-        }
-
-        void advance(std::ptrdiff_t n)
-        {
-            assert(n <= 0 || i_ + static_cast<std::size_t>(n) <= v_->size_);
-            assert(n >= 0 || static_cast<std::size_t>(-n) <= i_);
-
-            i_ += n;
-            if (i_ <= base_ && i_ - base_ < branching) {
-                curr_ += n;
-            } else {
-                base_ = i_ - (i_ & branching_mask);
-                curr_ = v_->array_for_(i_).begin() + (i_ - base_);
-            }
-        }
-
-        bool equal(const iterator& other) const
-        {
-            return i_ == other.i_;
-        }
-
-        std::ptrdiff_t distance_to(const iterator& other) const
-        {
-            return other.i_ > i_
-                ?   static_cast<ptrdiff_t>(other.i_ - i_)
-                : - static_cast<ptrdiff_t>(i_ - other.i_);
-        }
-
-        const T& dereference() const
-        {
-            return *curr_;
-        }
-    };
-
+    using iterator         = detail::vektor::iterator<T>;
     using const_iterator   = iterator;
     using reverse_iterator = std::reverse_iterator<iterator>;
 
-    vektor() : vektor{empty_} {}
-    vektor(const vektor&) = default;
-    vektor(vektor&&) = default;
-    vektor& operator=(const vektor&) = default;
-    vektor& operator=(vektor&&) = default;
+    vektor() = default;
 
-    iterator begin() const { return {this}; }
-    iterator end()   const { return {this, typename iterator::end_t{}}; }
+    iterator begin() const { return {&impl_}; }
+    iterator end()   const { return {&impl_, typename iterator::end_t{}}; }
 
     reverse_iterator rbegin() const { return reverse_iterator{end()}; }
     reverse_iterator rend()   const { return reverse_iterator{begin()}; }
 
-    std::size_t size() const { return size_; }
-    bool empty() const { return size_ == 0; }
+    std::size_t size() const { return impl_.size; }
+    bool empty() const { return impl_.size == 0; }
 
-    const T& operator[] (std::size_t index) const
-    {
-        const auto& arr = array_for_(index);
-        return arr [index & branching_mask];
-    }
+    reference operator[] (size_type index) const
+    { return impl_.get(index); }
 
-    vektor push_back(T value) const
-    {
-        auto tail_size = size_ - tail_offset_();
-        if (tail_size < branching) {
-            const auto& old_tail = tail_->leaf();
-            auto  new_tail_node  = make_node_(leaf_t{});
-            auto& new_tail       = new_tail_node->leaf();
-            std::copy(old_tail.begin(),
-                      old_tail.begin() + tail_size,
-                      new_tail.begin());
-            new_tail[tail_size] = std::move(value);
-            return vektor{ size_ + 1,
-                    shift_,
-                    root_,
-                    std::move(new_tail_node) };
-        } else {
-            auto new_tail_node = make_node_(leaf_t {{ std::move(value) }});
-            return ((size_ >> branching_log) > (1u << shift_))
-                ? vektor{ size_ + 1,
-                    shift_ + branching_log,
-                    make_node_(inner_t{{ root_, make_path_(shift_, tail_) }}),
-                    new_tail_node }
-                : vektor{ size_ + 1,
-                          shift_,
-                          push_tail_(shift_, *root_, tail_),
-                          new_tail_node };
-        }
-    }
+    vektor push_back(value_type value) const
+    { return { impl_.push_back(std::move(value)) }; }
 
 private:
-
-    std::size_t size_;
-    unsigned    shift_;
-    node_ptr    root_;
-    node_ptr    tail_;
+    vektor(detail::vektor::impl<T> impl) : impl_(std::move(impl)) {}
+    detail::vektor::impl<T> impl_ = detail::vektor::empty<T>;
 };
-
-template <typename T>
-const detail::vektor_node_ptr<T>
-vektor<T>::empty_inner_ = vektor<T>::make_node_(detail::vektor_inner<T>{});
-
-template <typename T>
-const detail::vektor_node_ptr<T>
-vektor<T>::empty_leaf_ = vektor<T>::make_node_(detail::vektor_leaf<T>{});
-
-template <typename T>
-const vektor<T>
-vektor<T>::empty_ = {0, branching_log, empty_inner_, empty_leaf_};
 
 } // namespace immu
