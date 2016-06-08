48ce2b22724e1c234a99a09168bf6a83b7fa4532
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-06-08 21:22:28 +0200

    Add basic benchmarking infrastructure

---
+++ benchmark/vektor-iter.cpp
@@ -0,0 +1,45 @@
+
+#include <nonius/nonius_single.h++>
+
+#include <immu/vektor.hpp>
+#include <vector>
+#include <list>
+#include <numeric>
+
+constexpr auto benchmark_size = 1000u;
+
+NONIUS_BENCHMARK("std::vector", [] (nonius::chronometer meter)
+{
+    auto v = std::vector<unsigned>{};
+    for (auto i = 0u; i < benchmark_size; ++i)
+        v.push_back(i);
+
+    meter.measure([&v] {
+        auto volatile x = std::accumulate(v.begin(), v.end(), 0u);
+        return x;
+    });
+})
+
+NONIUS_BENCHMARK("std::list", [] (nonius::chronometer meter)
+{
+    auto v = std::list<unsigned>{};
+    for (auto i = 0u; i < benchmark_size; ++i)
+        v.push_back(i);
+
+    meter.measure([&v] {
+        auto volatile x = std::accumulate(v.begin(), v.end(), 0u);
+        return x;
+    });
+})
+
+NONIUS_BENCHMARK("immu::vektor", [] (nonius::chronometer meter)
+{
+    auto v = immu::vektor<unsigned>{};
+    for (auto i = 0u; i < benchmark_size; ++i)
+        v = v.push_back(i);
+
+    meter.measure([&v] {
+        auto volatile x = std::accumulate(v.begin(), v.end(), 0u);
+        return x;
+    });
+})
