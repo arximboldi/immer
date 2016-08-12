e6072a17c2122c4a707098f008b0903c7352ce37
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-08-12 09:03:42 +0200

    Split vector nodes again into dvektor and vektor versions
    It is becoming clear that dvektor does not work so great in general and
    it adds too much complexity.  We are going to proceed with optimizations
    for `vektor` that require modifying the `node` stuff.

--- immu/detail/dvektor.hpp
+++ immu/detail/dvektor.hpp
@@ -1,13 +1,119 @@
 
 #pragma once
 
-#include <immu/detail/node.hpp>
+#include <immu/detail/free_list.hpp>
+#include <immu/detail/ref_count_base.hpp>
+
+#include <boost/intrusive_ptr.hpp>
 #include <boost/iterator/iterator_facade.hpp>
+#include <boost/smart_ptr/intrusive_ref_counter.hpp>
+
 #include <cassert>
 
 namespace immu {
 namespace detail {
 namespace dvektor {
+
+template <int B, typename T=std::size_t>
+constexpr T branches = T{1} << B;
+
+template <int B, typename T=std::size_t>
+constexpr T mask = branches<B, T> - 1;
+
+template <typename T, int B>
+struct node;
+
+template <typename T, int B>
+using node_ptr = boost::intrusive_ptr<node<T, B> >;
+
+template <typename T, int B>
+using leaf_node  = std::array<T, 1 << B>;
+
+template <typename T, int B>
+using inner_node = std::array<node_ptr<T, B>, 1 << B>;
+
+template <typename T, int B, typename Deriv=void>
+struct node_base : ref_count_base<Deriv>
+{
+    using leaf_node_t  = leaf_node<T, B>;
+    using inner_node_t = inner_node<T, B>;
+
+    enum
+    {
+        leaf_kind,
+        inner_kind
+    } kind;
+
+    union data_t
+    {
+        leaf_node_t  leaf;
+        inner_node_t inner;
+        data_t(leaf_node_t n)  : leaf(std::move(n)) {}
+        data_t(inner_node_t n) : inner(std::move(n)) {}
+        ~data_t() {}
+    } data;
+
+    ~node_base()
+    {
+        switch (kind) {
+        case leaf_kind:
+            data.leaf.~leaf_node_t();
+            break;
+        case inner_kind:
+            data.inner.~inner_node_t();
+            break;
+        }
+    }
+
+    node_base(leaf_node<T, B> n)
+        : kind{leaf_kind}
+        , data{std::move(n)}
+    {}
+
+    node_base(inner_node<T, B> n)
+        : kind{inner_kind}
+        , data{std::move(n)}
+    {}
+
+    inner_node_t& inner() & {
+        assert(kind == inner_kind);
+        return data.inner;
+    }
+    const inner_node_t& inner() const& {
+        assert(kind == inner_kind);
+        return data.inner;
+    }
+    inner_node_t&& inner() && {
+        assert(kind == inner_kind);
+        return std::move(data.inner);
+    }
+
+    leaf_node_t& leaf() & {
+        assert(kind == leaf_kind);
+        return data.leaf;
+    }
+    const leaf_node_t& leaf() const& {
+        assert(kind == leaf_kind);
+        return data.leaf;
+    }
+    leaf_node_t&& leaf() && {
+        assert(kind == leaf_kind);
+        return std::move(data.leaf);
+    }
+};
+
+template <typename T, int B>
+struct node : node_base<T, B, node<T, B>>
+            , with_thread_local_free_list<sizeof(node_base<T, B, node<T, B>>)>
+{
+    using node_base<T, B, node<T, B>>::node_base;
+};
+
+template <typename T, int B, typename ...Ts>
+auto make_node(Ts&& ...xs) -> boost::intrusive_ptr<node<T, B>>
+{
+    return new node<T, B>(std::forward<Ts>(xs)...);
+}
 
 auto fast_log2(std::size_t x)
 {
@@ -28,7 +134,7 @@ struct ref
     template <typename ...Ts>
     static auto make_node(Ts&& ...xs)
     {
-        return detail::make_node<T, B>(std::forward<Ts>(xs)...);
+        return dvektor::make_node<T, B>(std::forward<Ts>(xs)...);
     }
 
     const T& get_elem(std::size_t index, std::size_t xr) const
@@ -210,7 +316,7 @@ struct impl
     template <typename ...Ts>
     static auto make_node(Ts&& ...xs)
     {
-        return detail::make_node<T, B>(std::forward<Ts>(xs)...);
+        return dvektor::make_node<T, B>(std::forward<Ts>(xs)...);
     }
 
     void goto_pos_writable(std::size_t old_index,
