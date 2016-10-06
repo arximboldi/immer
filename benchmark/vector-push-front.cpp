3f464259755a116a62d52fc0601944caf269c019
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-10-06 16:09:52 +0200

    Add benchmarks for pushing at the front
    This is a good way to test concatenation performance.  For now, we just
    test our `flex_vector` against `librrb`.  As expected, we are slower
    since the code is a bit messy and there are lots of redundant bit
    shifting to compute sizes. Hopefully this will improve...

---
+++ benchmark/vector-push-front.cpp
@@ -0,0 +1,64 @@
+
+#include <nonius/nonius_single.h++>
+
+#include <immer/flex_vector.hpp>
+
+#include <immer/heap/gc_heap.hpp>
+#include <immer/refcount/no_refcount_policy.hpp>
+#include <immer/refcount/unsafe_refcount_policy.hpp>
+
+#if IMMER_BENCHMARK_LIBRRB
+extern "C" {
+#define restrict __restrict__
+#include <rrb.h>
+#undef restrict
+}
+#endif
+
+NONIUS_PARAM(N, std::size_t{1000})
+
+#if IMMER_BENCHMARK_LIBRRB
+NONIUS_BENCHMARK("librrb", [] (nonius::parameters params)
+{
+    auto n = params.get<N>();
+
+    return [=] {
+        auto v = rrb_create();
+        for (auto i = 0u; i < n; ++i) {
+            auto f = rrb_push(rrb_create(),
+                              reinterpret_cast<void*>(i));
+            v = rrb_concat(f, v);
+        }
+        return v;
+    };
+})
+#endif
+
+template <typename Vektor,
+          std::size_t Limit=std::numeric_limits<std::size_t>::max()>
+auto generic()
+{
+    return [] (nonius::parameters params)
+    {
+        auto n = params.get<N>();
+        if (n > Limit) n = 1;
+
+        return [=] {
+            auto v = Vektor{};
+            for (auto i = 0u; i < n; ++i)
+                v = v.push_front(i);
+            return v;
+        };
+    };
+};
+
+using gc_memory     = immer::memory_policy<immer::heap_policy<immer::gc_heap>, immer::no_refcount_policy>;
+using basic_memory  = immer::memory_policy<immer::heap_policy<immer::malloc_heap>, immer::refcount_policy>;
+using unsafe_memory = immer::memory_policy<immer::default_heap_policy, immer::unsafe_refcount_policy>;
+
+NONIUS_BENCHMARK("flex/4B", generic<immer::flex_vector<unsigned,4>>())
+NONIUS_BENCHMARK("flex/5B", generic<immer::flex_vector<unsigned,5>>())
+NONIUS_BENCHMARK("flex/6B", generic<immer::flex_vector<unsigned,6>>())
+NONIUS_BENCHMARK("flex/GC", generic<immer::flex_vector<unsigned,5,gc_memory>>())
+NONIUS_BENCHMARK("flex/NO", generic<immer::flex_vector<unsigned,5,basic_memory>>())
+NONIUS_BENCHMARK("flex/UN", generic<immer::flex_vector<unsigned,5,unsafe_memory>>())
