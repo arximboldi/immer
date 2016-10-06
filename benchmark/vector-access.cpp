0427cf16c90aecabcfbd60fefddc4639ea2a6374
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-10-06 16:58:22 +0200

    Merge iter/random benchmarks and try them on "smashed" rrbtrees

--- benchmark/vector-access.cpp
+++ benchmark/vector-access.cpp
@@ -23,6 +23,36 @@ extern "C" {
 
 NONIUS_PARAM(N, std::size_t{1000})
 
+struct push_back_fn
+{
+    template <typename T, typename U>
+    auto operator() (T&& v, U&& x)
+    {
+        return std::forward<T>(v)
+            .push_back(std::forward<U>(x));
+    }
+};
+
+struct push_front_fn
+{
+    template <typename T, typename U>
+    auto operator() (T&& v, U&& x)
+    {
+        return std::forward<T>(v)
+            .push_front(std::forward<U>(x));
+    }
+};
+
+auto make_generator(std::size_t runs)
+{
+    assert(runs > 0);
+    auto engine = std::default_random_engine{42};
+    auto dist = std::uniform_int_distribution<std::size_t>{0, runs-1};
+    auto r = std::vector<std::size_t>(runs);
+    std::generate_n(r.begin(), runs, std::bind(dist, engine));
+    return r;
+}
+
 NONIUS_BENCHMARK("std::vector", [] (nonius::parameters params)
 {
     auto n = params.get<N>();
@@ -47,6 +77,23 @@ NONIUS_BENCHMARK("std::vector/idx", [] (nonius::parameters params)
         auto r = 0u;
         for (auto i = 0u; i < n; ++i)
             r += v[i];
+        volatile auto rr = r;
+        return rr;
+    };
+})
+
+NONIUS_BENCHMARK("std::vector/random", [] (nonius::parameters params)
+{
+    auto n = params.get<N>();
+
+    auto g = make_generator(n);
+    auto v = std::vector<unsigned>(n);
+    std::iota(v.begin(), v.end(), 0u);
+
+    return [=] {
+        auto r = 0u;
+        for (auto i = 0u; i < n; ++i)
+            r += v[g[i]];
         volatile auto rr = r;
         return rr;
     };
@@ -83,18 +130,75 @@ NONIUS_BENCHMARK("librrb", [] (nonius::parameters params)
         return rr;
     };
 })
+
+NONIUS_BENCHMARK("librrb/F", [] (nonius::parameters params)
+{
+    auto n = params.get<N>();
+
+    auto v = rrb_create();
+    for (auto i = 0u; i < n; ++i) {
+        auto f = rrb_push(rrb_create(),
+                          reinterpret_cast<void*>(i));
+        v = rrb_concat(f, v);
+    }
+
+    return [=] {
+        auto r = 0u;
+        for (auto i = 0u; i < n; ++i)
+            r += reinterpret_cast<unsigned long>(rrb_nth(v, i));
+        volatile auto rr = r;
+        return rr;
+    };
+})
+
+NONIUS_BENCHMARK("librrb/random", [] (nonius::parameters params)
+{
+    auto n = params.get<N>();
+
+    auto v = rrb_create();
+    for (auto i = 0u; i < n; ++i)
+        v = rrb_push(v, reinterpret_cast<void*>(i));
+    auto g = make_generator(n);
+
+    return [=] {
+        auto r = 0u;
+        for (auto i = 0u; i < n; ++i)
+            r += reinterpret_cast<unsigned long>(rrb_nth(v, g[i]));
+        volatile auto rr = r;
+        return rr;
+    };
+})
+
+NONIUS_BENCHMARK("librrb/F/random", [] (nonius::parameters params)
+{
+    auto n = params.get<N>();
+
+    auto v = rrb_create();
+    for (auto i = 0u; i < n; ++i) {
+        auto f = rrb_push(rrb_create(),
+                          reinterpret_cast<void*>(i));
+        v = rrb_concat(f, v);
+    }
+
+    auto g = make_generator(n);
+
+    return [=] {
+        volatile auto r = 0u;
+        for (auto i = 0u; i < n; ++i)
+            r += reinterpret_cast<unsigned long>(rrb_nth(v, g[i]));
+        return r;
+    };
+})
 #endif
 
-template <typename Vektor,
-          std::size_t Limit=std::numeric_limits<std::size_t>::max()>
+template <typename Vektor>
 auto generic_iter()
 {
     return [] (nonius::parameters params)
     {
         auto n = params.get<N>();
-        if (n > Limit) n = 1;
-
         auto v = Vektor{};
+
         for (auto i = 0u; i < n; ++i)
             v = v.push_back(i);
 
@@ -108,7 +212,7 @@ auto generic_iter()
 NONIUS_BENCHMARK("vector/4B",   generic_iter<immer::vector<unsigned,4>>())
 NONIUS_BENCHMARK("vector/5B",   generic_iter<immer::vector<unsigned,5>>())
 NONIUS_BENCHMARK("vector/6B",   generic_iter<immer::vector<unsigned,6>>())
-NONIUS_BENCHMARK("array",       generic_iter<immer::array<unsigned>, 10000>())
+
 #if IMMER_BENCHMARK_EXPERIMENTAL
 NONIUS_BENCHMARK("dvektor/4B",  generic_iter<immer::dvektor<unsigned,4>>())
 NONIUS_BENCHMARK("dvektor/5B",  generic_iter<immer::dvektor<unsigned,5>>())
@@ -116,17 +220,16 @@ NONIUS_BENCHMARK("dvektor/6B",  generic_iter<immer::dvektor<unsigned,6>>())
 #endif
 
 template <typename Vektor,
-          std::size_t Limit=std::numeric_limits<std::size_t>::max()>
+          typename PushFn=push_back_fn>
 auto generic_idx()
 {
     return [] (nonius::parameters params)
     {
         auto n = params.get<N>();
-        if (n > Limit) n = 1;
 
         auto v = Vektor{};
         for (auto i = 0u; i < n; ++i)
-            v = v.push_back(i);
+            v = PushFn{}(std::move(v), i);
 
         return [=] {
             auto r = 0u;
@@ -139,6 +242,7 @@ auto generic_idx()
 };
 
 NONIUS_BENCHMARK("flex/5B/idx",     generic_idx<immer::flex_vector<unsigned,5>>())
+NONIUS_BENCHMARK("flex/F/5B/idx",   generic_idx<immer::flex_vector<unsigned,5>,push_front_fn>())
 NONIUS_BENCHMARK("vector/4B/idx",   generic_idx<immer::vector<unsigned,4>>())
 NONIUS_BENCHMARK("vector/5B/idx",   generic_idx<immer::vector<unsigned,5>>())
 NONIUS_BENCHMARK("vector/6B/idx",   generic_idx<immer::vector<unsigned,6>>())
@@ -149,17 +253,16 @@ NONIUS_BENCHMARK("dvektor/6B/idx",  generic_idx<immer::dvektor<unsigned,6>>())
 #endif
 
 template <typename Vektor,
-          std::size_t Limit=std::numeric_limits<std::size_t>::max()>
+          typename PushFn=push_back_fn>
 auto generic_reduce()
 {
     return [] (nonius::parameters params)
     {
         auto n = params.get<N>();
-        if (n > Limit) n = 1;
 
         auto v = Vektor{};
         for (auto i = 0u; i < n; ++i)
-            v = v.push_back(i);
+            v = PushFn{}(std::move(v), i);
 
         return [=] {
             auto volatile x = v.reduce(std::plus<unsigned>{}, 0u);
@@ -169,6 +272,41 @@ auto generic_reduce()
 }
 
 NONIUS_BENCHMARK("flex/5B/reduce",   generic_reduce<immer::flex_vector<unsigned,5>>())
+NONIUS_BENCHMARK("flex/F/5B/reduce", generic_reduce<immer::flex_vector<unsigned,5>,push_front_fn>())
 NONIUS_BENCHMARK("vector/4B/reduce", generic_reduce<immer::vector<unsigned,4>>())
 NONIUS_BENCHMARK("vector/5B/reduce", generic_reduce<immer::vector<unsigned,5>>())
 NONIUS_BENCHMARK("vector/6B/reduce", generic_reduce<immer::vector<unsigned,6>>())
+
+template <typename Vektor,
+          typename PushFn=push_back_fn>
+auto generic_random()
+{
+    return [] (nonius::parameters params)
+    {
+        auto n = params.get<N>();
+
+        auto v = Vektor{};
+        for (auto i = 0u; i < n; ++i)
+            v = PushFn{}(std::move(v), i);
+        auto g = make_generator(n);
+
+        return [=] {
+            auto r = 0u;
+            for (auto i = 0u; i < n; ++i)
+                r += v[g[i]];
+            volatile auto rr = r;
+            return rr;
+        };
+    };
+};
+
+NONIUS_BENCHMARK("flex/5B/random",     generic_random<immer::flex_vector<unsigned,5>>())
+NONIUS_BENCHMARK("flex/F/5B/random",   generic_random<immer::flex_vector<unsigned,5>, push_front_fn>())
+NONIUS_BENCHMARK("vector/4B/random",   generic_random<immer::vector<unsigned,4>>())
+NONIUS_BENCHMARK("vector/5B/random",   generic_random<immer::vector<unsigned,5>>())
+NONIUS_BENCHMARK("vector/6B/random",   generic_random<immer::vector<unsigned,6>>())
+#if IMMER_BENCHMARK_EXPERIMENTAL
+NONIUS_BENCHMARK("dvektor/4B/random",  generic_random<immer::dvektor<unsigned,4>>())
+NONIUS_BENCHMARK("dvektor/5B/random",  generic_random<immer::dvektor<unsigned,5>>())
+NONIUS_BENCHMARK("dvektor/6B/random",  generic_random<immer::dvektor<unsigned,6>>())
+#endif
---
+++ benchmark/vector-assoc.cpp
@@ -0,0 +1,236 @@
+
+#include <nonius/nonius_single.h++>
+
+#include <immer/vector.hpp>
+#include <immer/array.hpp>
+#include <immer/flex_vector.hpp>
+
+#if IMMER_BENCHMARK_EXPERIMENTAL
+#include <immer/experimental/dvektor.hpp>
+#endif
+
+#include <immer/heap/gc_heap.hpp>
+#include <immer/refcount/no_refcount_policy.hpp>
+#include <immer/refcount/unsafe_refcount_policy.hpp>
+
+#include <vector>
+#include <list>
+
+#if IMMER_BENCHMARK_LIBRRB
+extern "C" {
+#define restrict __restrict__
+#include <rrb.h>
+#undef restrict
+}
+#endif
+
+NONIUS_PARAM(N, std::size_t{1000})
+
+struct push_back_fn
+{
+    template <typename T, typename U>
+    auto operator() (T&& v, U&& x)
+    {
+        return std::forward<T>(v)
+            .push_back(std::forward<U>(x));
+    }
+};
+
+struct push_front_fn
+{
+    template <typename T, typename U>
+    auto operator() (T&& v, U&& x)
+    {
+        return std::forward<T>(v)
+            .push_front(std::forward<U>(x));
+    }
+};
+
+template <typename T>
+struct get_limit : std::integral_constant<
+    std::size_t, std::numeric_limits<std::size_t>::max()> {};
+
+template <typename T>
+struct get_limit<immer::array<T>> : std::integral_constant<
+    std::size_t, 10000> {};
+
+auto make_generator(std::size_t runs)
+{
+    assert(runs > 0);
+    auto engine = std::default_random_engine{42};
+    auto dist = std::uniform_int_distribution<std::size_t>{0, runs-1};
+    auto r = std::vector<std::size_t>(runs);
+    std::generate_n(r.begin(), runs, std::bind(dist, engine));
+    return r;
+}
+
+NONIUS_BENCHMARK("std::vector", [] (nonius::chronometer meter)
+{
+    auto n = meter.param<N>();
+
+    auto v = std::vector<unsigned>(n);
+    std::iota(v.begin(), v.end(), 0u);
+
+    auto all = std::vector<std::vector<unsigned>>(meter.runs(), v);
+
+    meter.measure([&] (int iter) {
+        auto& r = all[iter];
+        for (auto i = 0u; i < n; ++i)
+            r[i] = n - i;
+        return r;
+    });
+})
+
+NONIUS_BENCHMARK("std::vector/random", [] (nonius::chronometer meter)
+{
+    auto n = meter.param<N>();
+
+    auto g = make_generator(n);
+    auto v = std::vector<unsigned>(n);
+    std::iota(v.begin(), v.end(), 0u);
+
+    auto all = std::vector<std::vector<unsigned>>(meter.runs(), v);
+
+    meter.measure([&] (int iter) {
+        auto& r = all[iter];
+        for (auto i = 0u; i < n; ++i)
+            r[g[i]] = i;
+        return r;
+    });
+})
+
+#if IMMER_BENCHMARK_LIBRRB
+NONIUS_BENCHMARK("librrb", [] (nonius::chronometer meter)
+{
+    auto n = meter.param<N>();
+
+    auto v = rrb_create();
+    for (auto i = 0u; i < n; ++i)
+        v = rrb_push(v, reinterpret_cast<void*>(i));
+
+    meter.measure([&] {
+        auto r = v;
+        for (auto i = 0u; i < n; ++i)
+            r = rrb_update(r, i, reinterpret_cast<void*>(n - i));
+        return r;
+    });
+})
+
+NONIUS_BENCHMARK("librrb/F", [] (nonius::chronometer meter)
+{
+    auto n = meter.param<N>();
+
+    auto v = rrb_create();
+    for (auto i = 0u; i < n; ++i) {
+        auto f = rrb_push(rrb_create(),
+                          reinterpret_cast<void*>(i));
+        v = rrb_concat(f, v);
+    }
+
+    meter.measure([&] {
+        auto r = v;
+        for (auto i = 0u; i < n; ++i)
+            r = rrb_update(r, i, reinterpret_cast<void*>(n - i));
+        return r;
+    });
+})
+
+NONIUS_BENCHMARK("librrb/random", [] (nonius::chronometer meter)
+{
+    auto n = meter.param<N>();
+
+    auto g = make_generator(n);
+    auto v = rrb_create();
+    for (auto i = 0u; i < n; ++i)
+        v = rrb_push(v, reinterpret_cast<void*>(i));
+
+    meter.measure([&] {
+        auto r = v;
+        for (auto i = 0u; i < n; ++i)
+            r = rrb_update(r, g[i], reinterpret_cast<void*>(i));
+        return r;
+    });
+})
+#endif
+
+template <typename Vektor,
+          typename PushFn=push_back_fn>
+auto generic()
+{
+    return [] (nonius::chronometer meter)
+    {
+        auto n = meter.param<N>();
+        if (n > get_limit<Vektor>{}) n = 1;
+
+        auto v = Vektor{};
+        for (auto i = 0u; i < n; ++i)
+            v = PushFn{}(std::move(v), i);
+
+        meter.measure([&] {
+            auto r = v;
+            for (auto i = 0u; i < n; ++i)
+                r = v.assoc(i, n - i);
+            return r;
+        });
+    };
+};
+
+using gc_memory     = immer::memory_policy<immer::heap_policy<immer::gc_heap>, immer::no_refcount_policy>;
+using basic_memory  = immer::memory_policy<immer::heap_policy<immer::malloc_heap>, immer::refcount_policy>;
+using unsafe_memory = immer::memory_policy<immer::default_heap_policy, immer::unsafe_refcount_policy>;
+
+NONIUS_BENCHMARK("flex/5B",    generic<immer::flex_vector<unsigned,5>>())
+NONIUS_BENCHMARK("flex/F/5B",    generic<immer::flex_vector<unsigned,5>,push_front_fn>())
+
+NONIUS_BENCHMARK("vector/4B",  generic<immer::vector<unsigned,4>>())
+NONIUS_BENCHMARK("vector/5B",  generic<immer::vector<unsigned,5>>())
+NONIUS_BENCHMARK("vector/6B",  generic<immer::vector<unsigned,6>>())
+
+NONIUS_BENCHMARK("vector/GC",  generic<immer::vector<unsigned,5,gc_memory>>())
+NONIUS_BENCHMARK("vector/NO",  generic<immer::vector<unsigned,5,basic_memory>>())
+NONIUS_BENCHMARK("vector/UN",  generic<immer::vector<unsigned,5,unsafe_memory>>())
+
+#if IMMER_BENCHMARK_EXPERIMENTAL
+NONIUS_BENCHMARK("dvektor/4B", generic<immer::dvektor<unsigned,4>>())
+NONIUS_BENCHMARK("dvektor/5B", generic<immer::dvektor<unsigned,5>>())
+NONIUS_BENCHMARK("dvektor/6B", generic<immer::dvektor<unsigned,6>>())
+
+NONIUS_BENCHMARK("dvektor/GC", generic<immer::dvektor<unsigned,5,gc_memory>>())
+NONIUS_BENCHMARK("dvektor/NO", generic<immer::dvektor<unsigned,5,basic_memory>>())
+NONIUS_BENCHMARK("dvektor/UN", generic<immer::dvektor<unsigned,5,unsafe_memory>>())
+#endif
+
+NONIUS_BENCHMARK("array",      generic<immer::array<unsigned>>())
+
+template <typename Vektor>
+auto generic_random()
+{
+    return [] (nonius::parameters params)
+    {
+        auto n = params.get<N>();
+        if (n > get_limit<Vektor>{}) n = 1;
+
+        auto g = make_generator(n);
+        auto v = Vektor{};
+        for (auto i = 0u; i < n; ++i)
+            v = v.push_back(i);
+
+        return [=] {
+            auto r = v;
+            for (auto i = 0u; i < n; ++i)
+                r = v.assoc(g[i], i);
+            return r;
+        };
+    };
+};
+
+NONIUS_BENCHMARK("flex/5B/random",     generic_random<immer::flex_vector<unsigned,5>>())
+NONIUS_BENCHMARK("vector/4B/random",   generic_random<immer::vector<unsigned,4>>())
+NONIUS_BENCHMARK("vector/5B/random",   generic_random<immer::vector<unsigned,5>>())
+NONIUS_BENCHMARK("vector/6B/random",   generic_random<immer::vector<unsigned,6>>())
+#if IMMER_BENCHMARK_EXPERIMENTAL
+NONIUS_BENCHMARK("dvektor/4B/random",  generic_random<immer::dvektor<unsigned,4>>())
+NONIUS_BENCHMARK("dvektor/5B/random",  generic_random<immer::dvektor<unsigned,5>>())
+NONIUS_BENCHMARK("dvektor/6B/random",  generic_random<immer::dvektor<unsigned,6>>())
+#endif
+NONIUS_BENCHMARK("array/random",       generic_random<immer::array<unsigned>>())
