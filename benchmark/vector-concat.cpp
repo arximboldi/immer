a0cc9856dc7b1d8cc0e5dfc45fd574457ea7a49b
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-10-17 18:35:08 +0200

    Add benchmark for concatenation

---
+++ benchmark/vector-concat.cpp
@@ -0,0 +1,85 @@
+
+#include <nonius/nonius_single.h++>
+
+#include "util.hpp"
+
+#include <immer/flex_vector.hpp>
+
+#include <immer/heap/gc_heap.hpp>
+#include <immer/refcount/no_refcount_policy.hpp>
+#include <immer/refcount/unsafe_refcount_policy.hpp>
+
+#if IMMER_BENCHMARK_LIBRRB
+extern "C" {
+#define restrict __restrict__
+#include <rrb.h>
+#undef restrict
+}
+#endif
+
+NONIUS_PARAM(N, std::size_t{1000})
+
+#if IMMER_BENCHMARK_LIBRRB
+NONIUS_BENCHMARK("librrb", [] (nonius::chronometer meter)
+{
+    auto n = meter.param<N>();
+
+    auto v = rrb_create();
+    for (auto i = 0u; i < n; ++i)
+        v = rrb_push(v, reinterpret_cast<void*>(i));
+
+    meter.measure([&] {
+        return rrb_concat(v, v);
+    });
+})
+
+NONIUS_BENCHMARK("librrb/F", [] (nonius::chronometer meter)
+{
+    auto n = meter.param<N>();
+
+    auto v = rrb_create();
+    for (auto i = 0u; i < n; ++i) {
+        auto f = rrb_push(rrb_create(),
+                          reinterpret_cast<void*>(i));
+        v = rrb_concat(f, v);
+    }
+
+    meter.measure([&] {
+        return rrb_concat(v, v);
+    });
+})
+#endif
+
+template <typename Vektor,
+          typename PushFn=push_back_fn>
+auto generic()
+{
+    return [] (nonius::chronometer meter)
+    {
+        auto n = meter.param<N>();
+
+        auto v = Vektor{};
+        for (auto i = 0u; i < n; ++i)
+            v = PushFn{}(std::move(v), i);
+
+        meter.measure([&] {
+            return v + v;
+        });
+    };
+};
+
+using gc_memory     = immer::memory_policy<immer::heap_policy<immer::gc_heap>, immer::no_refcount_policy>;
+using basic_memory  = immer::memory_policy<immer::heap_policy<immer::malloc_heap>, immer::refcount_policy>;
+using unsafe_memory = immer::memory_policy<immer::default_heap_policy, immer::unsafe_refcount_policy>;
+
+NONIUS_BENCHMARK("flex/4B", generic<immer::flex_vector<unsigned,4>>())
+NONIUS_BENCHMARK("flex/5B", generic<immer::flex_vector<unsigned,5>>())
+NONIUS_BENCHMARK("flex/6B", generic<immer::flex_vector<unsigned,6>>())
+NONIUS_BENCHMARK("flex/GC", generic<immer::flex_vector<unsigned,5,gc_memory>>())
+NONIUS_BENCHMARK("flex_s/GC", generic<immer::flex_vector<std::size_t,5,gc_memory>>())
+NONIUS_BENCHMARK("flex/NO", generic<immer::flex_vector<unsigned,5,basic_memory>>())
+NONIUS_BENCHMARK("flex/UN", generic<immer::flex_vector<unsigned,5,unsafe_memory>>())
+
+NONIUS_BENCHMARK("flex/F/5B", generic<immer::flex_vector<unsigned,5>,push_front_fn>())
+NONIUS_BENCHMARK("flex/F/GC", generic<immer::flex_vector<unsigned,5,gc_memory>,push_front_fn>())
+NONIUS_BENCHMARK("flex_s/F/GC", generic<immer::flex_vector<std::size_t,5,gc_memory>,push_front_fn>())
