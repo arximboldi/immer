e6e3c3f7d8ed4ade23928c3db56ca90108d02b85
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-11-12 12:23:53 +0100

    Add hack to fix issues with breathe/sphinx
    https://github.com/michaeljones/breathe/issues/284

---
+++ doc/sphinx-html-hack.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+location=`dirname $0`
+
+# Fixes issues described here:
+# https://github.com/michaeljones/breathe/issues/284
+
+src='<span id="\([^"]*\)"></span>\(.*\)<em class="property">class </em>'
+dst='<span id=""></span>\2<em class="property">class</em><tt class="descname">\1</tt>'
+sed -i "s@$src@$dst@g" $location/_build/html/*.html
+
+src='<span id="\([^"]*\)"></span>\(.*\)<em class="property">struct </em>'
+dst='<span id=""></span>\2<em class="property">struct</em><tt class="descname">\1</tt>'
+sed -i "s@$src@$dst@g" $location/_build/html/*.html
+
+src='<em class="property">using </em><em class="property">using </em>'
+dst='<em class="property">using </em>'
+sed -i "s@$src@$dst@g" $location/_build/html/*.html
+
+src='<code class="descclassname">\([^&]*\)&lt;\([^&]*\)&gt;::</code>'
+dst='<code class="descclassname">\1::</code>'
+sed -i "s@$src@$dst@g" $location/_build/html/*.html
