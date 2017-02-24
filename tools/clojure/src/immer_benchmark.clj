2a25692811098b3446755b5d13ebd68a1ffc15b4
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2017-02-24 20:15:54 +0100

    Tweak Clojure benchmarks

--- tools/clojure/src/immer_benchmark.clj
+++ tools/clojure/src/immer_benchmark.clj
@@ -2,75 +2,105 @@
   (:require [criterium.core :as c]
             [clojure.core.rrb-vector :as fv]))
 
-(def benchmark-size 100000)
-(def benchmark-samples 20)
+(defn h0 [& args]
+  (apply println "\n####" args))
 
-(defn vector-push-f [v]
-  (loop [v v
-         i 0]
-    (if (< i benchmark-size)
-      (recur (fv/catvec (fv/vector i) v)
-             (inc i))
-      v)))
+(defn h1 [& args]
+  (apply println "\n====" args))
 
-(defn vector-push [v]
-  (loop [v v
-         i 0]
-    (if (< i benchmark-size)
-      (recur (conj v i)
-             (inc i))
-      v)))
+(defn h2 [& args]
+  (apply println "\n----" args))
 
-(defn vector-concat [v vc]
-  (loop [v v
-         i 0]
-    (if (< i benchmark-size)
-      (recur (fv/catvec v vc)
-             (inc i))
-      v)))
+(defn run-benchmarks [N S]
+  (h0 "Running benchmarks:  N =" N " S =" S)
 
-(defn vector-update [v]
-  (loop [v v
-         i 0]
-    (if (< i benchmark-size)
-      (recur (assoc v i (+ i 1))
-             (inc i))
-      v)))
+  (def c-steps 10)
 
-(defn vector-iter [v]
-  (reduce + 0 v))
+  (defn vector-push-f [v]
+    (loop [v v
+           i 0]
+      (if (< i N)
+        (recur (fv/catvec (fv/vector i) v)
+               (inc i))
+        v)))
 
-(def benchmark-vector (vec (range benchmark-size)))
-(def benchmark-rrb-vector (fv/vec (range benchmark-size)))
-(def benchmark-rrb-vector-short (fv/vec (range (/ benchmark-size 2))))
-(def benchmark-rrb-vector-f (vector-push-f (fv/vector)))
+  (defn vector-push [v]
+    (loop [v v
+           i 0]
+      (if (< i N)
+        (recur (conj v i)
+               (inc i))
+        v)))
+
+  (defn vector-push! [v]
+    (loop [v (transient v)
+           i 0]
+      (if (< i N)
+        (recur (conj! v i)
+               (inc i))
+        (persistent! v))))
+
+  (defn vector-concat [v vc]
+    (loop [v v
+           i 0]
+      (if (< i c-steps)
+        (recur (fv/catvec v vc)
+               (inc i))
+        v)))
+
+  (defn vector-update [v]
+    (loop [v v
+           i 0]
+      (if (< i N)
+        (recur (assoc v i (+ i 1))
+               (inc i))
+        v)))
+
+  (defn vector-update! [v]
+    (loop [v (transient v)
+           i 0]
+      (if (< i N)
+        (recur (assoc! v i (+ i 1))
+               (inc i))
+        (persistent! v))))
+
+  (defn vector-iter [v]
+    (reduce + 0 v))
+
+  (defn the-benchmarks [empty-v]
+    (def full-v (into empty-v (range N)))
+    (h2 "iter")
+    (c/bench (vector-iter full-v) :samples S)
+    (h2 "update!")
+    (c/bench (vector-update! full-v) :samples S)
+    (h2 "push!")
+    (c/bench (vector-push! empty-v) :samples S)
+    (h2 "update")
+    (c/bench (vector-update full-v) :samples S)
+    (h2 "push")
+    (c/bench (vector-push empty-v) :samples S))
+
+  (defn the-rrb-benchmarks [empty-v]
+    (if (> N 1000)
+      (h2 "skipping relaxed test 'cuz N > 1000 (rrb-vector bug)")
+      (do
+        (def full-v (vector-push-f empty-v))
+        (h2 "iter/f")
+        (c/bench (vector-iter full-v) :samples S)
+        (h2 "update/f")
+        (c/bench (vector-update full-v) :samples S)))
+    (def short-v (into empty-v (range (/ N c-steps))))
+    (h2 "concat")
+    (c/bench (vector-concat empty-v short-v)) :samples S)
+
+  (h1 "vector")
+  (the-benchmarks (vector))
+
+  (h1 "rrb-vector")
+  (the-benchmarks (fv/vector))
+  (the-rrb-benchmarks (fv/vector)))
 
 (defn -main []
-  (println "\n== VECTOR ITER ================")
-  (c/with-progress-reporting (c/bench (vector-iter benchmark-vector)
-                                      :samples benchmark-samples))
-  (println "\n== VECTOR PUSH ================")
-  (c/with-progress-reporting (c/bench (vector-push (vector))
-                                      :samples benchmark-samples))
-  (println "\n== VECTOR UPDATE ================")
-  (c/with-progress-reporting (c/bench (vector-update benchmark-vector)
-                                      :samples benchmark-samples))
-  (println "\n== RRB-VECTOR ITER ================")
-  (c/with-progress-reporting (c/bench (vector-iter benchmark-rrb-vector)
-                                      :samples benchmark-samples))
-  (println "\n== RRB-VECTOR PUSH ================")
-  (c/with-progress-reporting (c/bench (vector-push (fv/vector))
-                                      :samples benchmark-samples))
-  (println "\n== RRB-VECTOR UPDATE ================")
-  (c/with-progress-reporting (c/bench (vector-update benchmark-rrb-vector)
-                                      :samples benchmark-samples))
-  (println "\n== RRB-VECTOR CONCAT ================")
-  (c/with-progress-reporting (c/bench (vector-concat (fv/vector)
-                                                     benchmark-rrb-vector-short)
-                                      :samples benchmark-samples))
-  (println "\n== RRB-VECTOR/F ITER ================")
-  (c/with-progress-reporting (c/bench (vector-iter benchmark-rrb-vector-f)
-                                      :samples benchmark-samples))
-  (println "\n== RRB-VECTOR/F UPDATE ================")
-  (c/with-progress-reporting (c/bench (vector-update benchmark-rrb-vector-f)
-                                      :samples benchmark-samples)))
+  (run-benchmarks 1000 100)
+  (run-benchmarks 100000 20)
+  (run-benchmarks 10000000 3))
