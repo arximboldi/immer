205c78cfbf5fbff07e5df15c5f2daa7a90b946f9
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2017-04-19 20:23:53 +0200

    Update sphinx hacks

--- doc/sphinx-html-hack.bash
+++ doc/sphinx-html-hack.bash
@@ -4,43 +4,73 @@ location=`dirname $0`
 
 echo "Running $0 at $location"
 
-# Fixes issues described here:
+# Fixes issues described here among others
 # https://github.com/michaeljones/breathe/issues/284
 
-src='<span id="\([^"]*\)"></span>\(.*\)<em class="property">class </em>'
-dst='<span id=""></span>\2<em class="property">class</em><tt class="descname">\1</tt>'
-sed -i "s@$src@$dst@g" $location/_build/html/*.html
+fix-missing-class-name()
+{
+    src='<span id="\([^"]*\)"></span>\(.*\)<em class="property">class </em>'
+    dst='<span id=""></span>\2<em class="property">class</em><tt class="descname">\1</tt>'
+    sed -i "s@$src@$dst@g" $location/_build/html/*.html
+}
 
-src='<span id="\([^"]*\)"></span>\(.*\)<em class="property">struct </em>'
-dst='<span id=""></span>\2<em class="property">struct</em><tt class="descname">\1</tt>'
-sed -i "s@$src@$dst@g" $location/_build/html/*.html
+fix-missing-struct-name()
+{
+    src='<span id="\([^"]*\)"></span>\(.*\)<em class="property">struct </em>'
+    dst='<span id=""></span>\2<em class="property">struct</em><tt class="descname">\1</tt>'
+    sed -i "s@$src@$dst@g" $location/_build/html/*.html
+}
 
-src='<em class="property">using</em><em class="property">using </em>'
-dst='<em class="property">using </em>'
-sed -i "s@$src@$dst@g" $location/_build/html/*.html
+fix-double-using-keyword()
+{
+    src='<em class="property">using</em><em class="property">using </em>'
+    dst='<em class="property">using </em>'
+    sed -i "s@$src@$dst@g" $location/_build/html/*.html
+}
 
-src='<em class="property">using </em><em class="property">using </em>'
-dst='<em class="property">using </em>'
-sed -i "s@$src@$dst@g" $location/_build/html/*.html
+fix-do-not-repeat-type-in-member-using-declaration()
+{
+    src='<em class="property">using </em><code class="descname">\(\([^:]*::\)*\)\([^ ]*\) = \([^<]*\)</code>'
+    dst='<em class="property">using </em><code class="descname">\3 = \4</code>'
+    sed -i "s@$src@$dst@g" $location/_build/html/*.html
+}
 
-src='<em class="property">using </em><code class="descname">\(\([^:]*::\)*\)\([^ ]*\) = \([^<]*\)</code>'
-dst='<em class="property">using </em><code class="descname">\3 = \4</code>'
-sed -i "s@$src@$dst@g" $location/_build/html/*.html
+fix-remove-double-class-name()
+{
+    # src='<code class="descclassname">\([^&]*\)&lt;\([^&]*\)&gt;::</code>'
+    # dst='<code class="descclassname">\1::</code>'
+    src='<code class="descclassname">\([^<]*\)</code>'
+    dst=''
+    sed -i "s@$src@$dst@g" $location/_build/html/*.html
+}
 
-# src='<code class="descclassname">\([^&]*\)&lt;\([^&]*\)&gt;::</code>'
-# dst='<code class="descclassname">\1::</code>'
-src='<code class="descclassname">\([^<]*\)</code>'
-dst=''
-sed -i "s@$src@$dst@g" $location/_build/html/*.html
+fix-remove-straneous-typedefs()
+{
+    src='typedef '
+    dst=''
+    sed -i "s@$src@$dst@g" $location/_build/html/*.html
+}
 
-src='breathe-sectiondef container'
-dst=''
-sed -i "s@$src@$dst@g" $location/_build/html/*.html
+fix-remove-straneous-using-declarations()
+{
+    src='<em class="property">using </em>template&lt;&gt;<br />'
+    dst=''
+    sed -i "s@$src@$dst@g" $location/_build/html/*.html
+}
 
-src='typedef '
-dst=''
-sed -i "s@$src@$dst@g" $location/_build/html/*.html
+fix-remove-straneous-template-in-using-declarations()
+{
+    src='\(<dl class="type">\n<dt[^>]*>\)\ntemplate&lt;&gt;<br />'
+    dst='\1'
+    pre=':a;N;$!ba;'
+    sed -i "$pre;s@$src@$dst@g" $location/_build/html/*.html
+}
+fix-remove-straneous-template-in-using-declarations
 
-src='<em class="property">using </em>template&lt;&gt;<br />'
-dst=''
-sed -i "s@$src@$dst@g" $location/_build/html/*.html
+fix-remove-countainer-css-class-in-member-definitions-causing-overflow()
+{
+    src='breathe-sectiondef\([[:alnum:] _-]*\)container'
+    dst='breathe-sectiondef'
+    sed -i "s@$src@$dst@g" $location/_build/html/*.html
+}
+fix-remove-countainer-css-class-in-member-definitions-causing-overflow
