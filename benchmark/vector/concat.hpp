2136d4260906c97ac8a9e46674bce2d492ec14bc
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2017-06-05 23:31:42 +0200

    Resort to older transient concatenation
    The newer one was incorrectly giving the result that the transient is
    slower, because of damaged cache locality.

--- benchmark/vector/concat.hpp
+++ benchmark/vector/concat.hpp
@@ -24,6 +24,8 @@
 
 namespace {
 
+constexpr auto concat_steps = 10u;
+
 template <typename Vektor,
           typename PushFn=push_back_fn>
 auto benchmark_concat()
@@ -62,16 +64,15 @@ auto benchmark_concat_incr()
     return
         [] (nonius::chronometer meter)
         {
-            auto steps = 10u;
             auto n = meter.param<N>();
 
             auto v = Vektor{};
-            for (auto i = 0u; i < n / steps; ++i)
+            for (auto i = 0u; i < n / concat_steps; ++i)
                 v = PushFn{}(std::move(v), i);
 
             measure(meter, [&] {
                     auto r = Vektor{};
-                    for (auto i = 0u; i < steps; ++i)
+                    for (auto i = 0u; i < concat_steps; ++i)
                         r = r + v;
                     return r;
                 });
@@ -84,17 +85,37 @@ auto benchmark_concat_incr_mut()
     return
         [] (nonius::chronometer meter)
         {
-            constexpr auto steps = 10u;
+            auto n = meter.param<N>();
+
+            auto v = Vektor{}.transient();
+            for (auto i = 0u; i < n / concat_steps; ++i)
+                v.push_back(i);
+
+            measure(meter, [&] (int run) {
+                auto r = Vektor{}.transient();
+                for (auto i = 0u; i < concat_steps; ++i)
+                    r.append(v);
+                return r;
+            });
+        };
+};
+
+template <typename Vektor>
+auto benchmark_concat_incr_mut2()
+{
+    return
+        [] (nonius::chronometer meter)
+        {
             auto n = meter.param<N>();
 
             using transient_t = typename Vektor::transient_type;
             using steps_t = std::vector<transient_t, gc_allocator<transient_t>>;
             auto vs = std::vector<steps_t, gc_allocator<steps_t>>(meter.runs());
             for (auto k = 0u; k < vs.size(); ++k) {
-                vs[k].reserve(steps);
-                for (auto j = 0u; j < steps; ++j) {
+                vs[k].reserve(concat_steps);
+                for (auto j = 0u; j < concat_steps; ++j) {
                     auto vv = Vektor{}.transient();
-                    for (auto i = 0u; i < n / steps; ++i)
+                    for (auto i = 0u; i < n / concat_steps; ++i)
                         vv.push_back(i);
                     vs[k].push_back(std::move(vv));
                 }
@@ -102,8 +123,8 @@ auto benchmark_concat_incr_mut()
             measure(meter, [&] (int run) {
                 auto& vr = vs[run];
                 auto r = Vektor{}.transient();
-                assert(vr.size() == steps);
-                for (auto i = 0u; i < steps; ++i)
+                assert(vr.size() == concat_steps);
+                for (auto i = 0u; i < concat_steps; ++i)
                     r.append(std::move(vr[i]));
                 return r;
             });
@@ -116,15 +137,14 @@ auto benchmark_concat_incr_chunkedseq()
     return
         [] (nonius::chronometer meter)
         {
-            constexpr auto steps = 10u;
             auto n = meter.param<N>();
 
             using steps_t = std::vector<Vektor>;
             auto vs = std::vector<steps_t>(meter.runs());
             for (auto k = 0u; k < vs.size(); ++k) {
-                for (auto j = 0u; j < steps; ++j) {
+                for (auto j = 0u; j < concat_steps; ++j) {
                     auto vv = Vektor{};
-                    for (auto i = 0u; i < n / steps; ++i)
+                    for (auto i = 0u; i < n / concat_steps; ++i)
                         vv.push_back(i);
                     vs[k].push_back(std::move(vv));
                 }
@@ -132,7 +152,7 @@ auto benchmark_concat_incr_chunkedseq()
             measure(meter, [&] (int run) {
                 auto& vr = vs[run];
                 auto r = Vektor{};
-                for (auto i = 0u; i < steps; ++i)
+                for (auto i = 0u; i < concat_steps; ++i)
                     r.concat(vr[i]);
                 return r;
             });
@@ -145,11 +165,10 @@ auto benchmark_concat_incr_librrb(Fn maker)
     return
         [=] (nonius::chronometer meter) {
             auto n = meter.param<N>();
-            auto steps = 10u;
-            auto v = maker(n / steps);
+            auto v = maker(n / concat_steps);
             measure(meter, [&] {
                     auto r = rrb_create();
-                    for (auto i = 0ul; i < steps; ++i)
+                    for (auto i = 0ul; i < concat_steps; ++i)
                         r = rrb_concat(r, v);
                     return r;
                 });
