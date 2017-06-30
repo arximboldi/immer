f63d9115f91205d2db79c8d79c35ced5df1b50d4
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2017-06-30 15:41:07 +0200

    Improve Guile benchmarks
    Thanks to Andy Wingo for the feedback!

--- extra/guile/benchmark.scm
+++ extra/guile/benchmark.scm
@@ -17,10 +17,12 @@
 ;; along with immer.  If not, see <http://www.gnu.org/licenses/>.
 
 (use-modules (immer)
+             (fector) ; https://wingolog.org/pub/fector.scm
              (srfi srfi-1)
              (srfi srfi-43)
              (ice-9 vlist)
-             (ice-9 pretty-print))
+             (ice-9 pretty-print)
+             (rnrs bytevectors))
 
 (define-syntax display-eval
   (syntax-rules ()
@@ -64,12 +66,19 @@
 
 (display ";;;; benchmarking creation...") (newline)
 
+(display-eval
+ (define (fector . args)
+   (persistent-fector (fold (lambda (v fv) (fector-push! fv v))
+                            (transient-fector)
+                            args))))
+
 (benchmark (apply ivector (iota bench-size)))
 (benchmark (apply ivector-u32 (iota bench-size)))
 (benchmark (iota bench-size))
 (benchmark (apply vector (iota bench-size)))
 (benchmark (apply u32vector (iota bench-size)))
 (benchmark (list->vlist (iota bench-size)))
+(benchmark (apply fector (iota bench-size)))
 
 (display ";;;; benchmarking iteration...") (newline)
 
@@ -79,39 +88,87 @@
 (display-eval (define bench-vector (apply vector (iota bench-size))))
 (display-eval (define bench-u32vector (apply u32vector (iota bench-size))))
 (display-eval (define bench-vlist (list->vlist (iota bench-size))))
+(display-eval (define bench-fector (apply fector (iota bench-size))))
+(display-eval (define bench-bytevector-u32
+                (uint-list->bytevector (iota bench-size)
+                                       (native-endianness)
+                                       4)))
 
 (benchmark (ivector-fold + 0 bench-ivector))
 (benchmark (ivector-u32-fold + 0 bench-ivector-u32))
 (benchmark (fold + 0 bench-list))
 (benchmark (vector-fold + 0 bench-vector))
 (benchmark (vlist-fold + 0 bench-vlist))
+(benchmark (fector-fold + bench-fector 0))
 
 (display ";;;; benchmarking iteration by index...") (newline)
-(benchmark (let iter ((i 0) (acc 0))
-             (if (< i (ivector-length bench-ivector))
-                 (iter (+ i 1)
-                       (+ acc (ivector-ref bench-ivector i)))
-                 acc)))
-(benchmark (let iter ((i 0) (acc 0))
-             (if (< i (ivector-u32-length bench-ivector-u32))
-                 (iter (+ i 1)
-                       (+ acc (ivector-u32-ref bench-ivector-u32 i)))
-                 acc)))
-(benchmark (let iter ((i 0) (acc 0))
-             (if (< i (vector-length bench-vector))
-                 (iter (+ i 1)
-                       (+ acc (vector-ref bench-vector i)))
-                 acc)))
-(benchmark (let iter ((i 0) (acc 0))
-             (if (< i (u32vector-length bench-u32vector))
-                 (iter (+ i 1)
-                       (+ acc (u32vector-ref bench-u32vector i)))
-                 acc)))
-(benchmark (let iter ((i 0) (acc 0))
-             (if (< i (vlist-length bench-vlist))
-                 (iter (+ i 1)
-                       (+ acc (vlist-ref bench-vlist i)))
-                 acc)))
+
+(display-eval
+ (define-syntax iteration-by-index
+   (syntax-rules ()
+     ((_ *length *ref *vector *step)
+      (let ((len (*length *vector)))
+        (let iter ((i 0) (acc 0))
+          (if (< i len)
+              (iter (+ i *step)
+                    (+ acc (*ref *vector i)))
+              acc)))))))
+
+(display-eval
+ (define-syntax iteration-by-index-truncate
+   (syntax-rules ()
+     ((_ *length *ref *vector *step)
+      (let ((len (*length *vector)))
+        (let iter ((i 0) (acc 0))
+          (if (< i len)
+              (iter (+ i *step)
+                    (logand #xffffffffFFFFFFFF
+                            (+ acc (*ref *vector i))))
+              acc)))))))
+
+(benchmark (iteration-by-index ivector-length
+                               ivector-ref
+                               bench-ivector 1))
+(benchmark (iteration-by-index ivector-u32-length
+                               ivector-u32-ref
+                               bench-ivector-u32 1))
+(benchmark (iteration-by-index vector-length
+                               vector-ref
+                               bench-vector 1))
+(benchmark (iteration-by-index u32vector-length
+                               u32vector-ref
+                               bench-u32vector 1))
+(benchmark (iteration-by-index vlist-length
+                               vlist-ref
+                               bench-vlist 1))
+(benchmark (iteration-by-index fector-length
+                               fector-ref
+                               bench-fector 1))
+(benchmark (iteration-by-index bytevector-length
+                               bytevector-u32-native-ref
+                               bench-bytevector-u32 4))
+
+(benchmark (iteration-by-index-truncate ivector-length
+                                        ivector-ref
+                                        bench-ivector 1))
+(benchmark (iteration-by-index-truncate ivector-u32-length
+                                        ivector-u32-ref
+                                        bench-ivector-u32 1))
+(benchmark (iteration-by-index-truncate vector-length
+                                        vector-ref
+                                        bench-vector 1))
+(benchmark (iteration-by-index-truncate u32vector-length
+                                        u32vector-ref
+                                        bench-u32vector 1))
+(benchmark (iteration-by-index-truncate vlist-length
+                                        vlist-ref
+                                        bench-vlist 1))
+(benchmark (iteration-by-index-truncate fector-length
+                                        fector-ref
+                                        bench-fector 1))
+(benchmark (iteration-by-index-truncate bytevector-length
+                                        bytevector-u32-native-ref
+                                        bench-bytevector-u32 4))
 
 (display ";;;; benchmarking concatenation...") (newline)
 (benchmark (ivector-append bench-ivector bench-ivector))
