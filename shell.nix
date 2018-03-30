7dd74f63617355417116f9f71e0e66679fd6973b
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2018-03-30 18:51:09 +0200

    Upgrade Nixpkgs
    We also change the way we set the compiler inside the Nix shell to a
    more resilient mechanism.

--- shell.nix
+++ shell.nix
@@ -2,25 +2,27 @@
   nixpkgs ? (import <nixpkgs> {}).fetchFromGitHub {
     owner  = "NixOS";
     repo   = "nixpkgs";
-    rev    = "d0d905668c010b65795b57afdf7f0360aac6245b";
-    sha256 = "1kqxfmsik1s1jsmim20n5l4kq6wq8743h5h17igfxxbbwwqry88l";
+    rev    = "adaa9d93488c7fc1fee020899cb08ce2f899c94b";
+    sha256 = "0rkfxgmdammpq4xbsx7994bljqhzjjblpky2pqmkgn6x9s6adwrr";
   }}:
 
 with import nixpkgs {};
 
 let
-  compiler_pkg       = if compiler != ""
-                       then pkgs.${compiler}
-                       else stdenv.cc;
-  docs               = import ./nix/docs.nix { inherit nixpkgs; };
-  benchmarks         = import ./nix/benchmarks.nix { inherit nixpkgs; };
-  propagate_compiler = compiler_pkg.isClang != stdenv.cc.isClang;
-  native_compiler    = compiler_pkg.isClang == stdenv.cc.isClang;
+  docs        = import ./nix/docs.nix { inherit nixpkgs; };
+  benchmarks  = import ./nix/benchmarks.nix { inherit nixpkgs; };
+  compilerPkg = if compiler != ""
+                then pkgs.${compiler}
+                else stdenv.cc;
+  theStdenv   = if compilerPkg.isClang
+                then clangStdenv
+                else stdenv;
 in
 
-stdenv.mkDerivation rec {
+theStdenv.mkDerivation rec {
   name = "immer-env";
   buildInputs = [
+    compilerPkg
     git
     cmake
     pkgconfig
@@ -41,12 +43,9 @@ stdenv.mkDerivation rec {
       docs.breathe
       docs.recommonmark
     ]))
-  ]
-  ++ stdenv.lib.optionals compiler_pkg.isClang [
-    llvm libcxx libcxxabi
   ];
-  propagatedBuildInputs =
-    stdenv.lib.optional propagate_compiler compiler_pkg;
-  nativeBuildInputs =
-    stdenv.lib.optional native_compiler compiler_pkg;
+  shellHook = ''
+    export CC=${compilerPkg}/bin/cc
+    export CXX=${compilerPkg}/bin/c++
+  '';
 }
