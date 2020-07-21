9d571430e8cc4e265e5d3beacc4863f1b2d941af
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2020-07-21 17:31:32 +0200

    Attempt to reproduce 24168

--- test/oss-fuzz/flex-vector-gc-0.cpp
+++ test/oss-fuzz/flex-vector-gc-0.cpp
@@ -210,6 +210,16 @@ int run_input(const std::uint8_t* data, std::size_t size)
 
 } // namespace
 
+TEST_CASE("https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=24168")
+{
+    SECTION("fuzzer")
+    {
+        auto input = load_input(
+            "clusterfuzz-testcase-minimized-flex-vector-gc-5428967461617664");
+        CHECK(run_input(input.data(), input.size()) == 0);
+    }
+}
+
 TEST_CASE("https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=24148")
 {
     SECTION("fuzzer")
