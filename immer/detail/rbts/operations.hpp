arximboldi/immer

commit ad9f504e0bcce94e7d7ff9ff4f22aa46fa07af5a

Fix subtle bug producing too much rebalancing in flex vector
This bug has been inherited from c-rrb.

immer/detail/rbts/operations.hpp
@@ -1731,16 +1731,18 @@ struct concat_rebalance_plan
         const auto branches             = count_t{1} << bits;
         const auto optimal              = ((total - 1) >> bits) + 1;
         count_t i                       = 0;
-        while (n >= optimal + rrb_extras) {
+        while (n > optimal + rrb_extras) {
             // skip ok nodes
-            while (counts[i] > branches - rrb_invariant)
+            while (counts[i] >= branches - rrb_invariant)
                 i++;
+            assert(i < n);
             // short node, redistribute
             auto remaining = counts[i];
             do {
-                auto count = std::min(remaining + counts[i + 1], branches);
+                auto next  = counts[i + 1];
+                auto count = std::min(remaining + next, branches);
                 counts[i]  = count;
-                remaining += counts[i + 1] - count;
+                remaining += next - count;
                 ++i;
             } while (remaining > 0);
             // remove node

commit 7b2e1db783eddb55e7ba8a469307a253f877724c

Fix crash 5078027885871104
This was actually introduced by
ad9f504e0bcce94e7d7ff9ff4f22aa46fa07af5a, which was actually a
misinterpretation of the code.

immer/detail/rbts/operations.hpp
@@ -1751,9 +1751,9 @@ struct concat_rebalance_plan
         const auto branches             = count_t{1} << bits;
         const auto optimal              = ((total - 1) >> bits) + 1;
         count_t i                       = 0;
-        while (n > optimal + rrb_extras) {
+        while (n >= optimal + rrb_extras) {
             // skip ok nodes
-            while (counts[i] >= branches - rrb_invariant)
+            while (counts[i] > branches - rrb_invariant)
                 i++;
             assert(i < n);
             // short node, redistribute

test/oss-fuzz/flex-vector-0.cpp
@@ -258,3 +258,14 @@ TEST_CASE("https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=24147")
         CHECK(run_input(input.data(), input.size()) == 0);
     }
 }
+
+TEST_CASE("https://oss-fuzz.com/testcase-detail/5078027885871104")
+{
+    SECTION("fuzzer")
+    {
+        return;
+        auto input = load_input(
+            "clusterfuzz-testcase-minimized-flex-vector-5078027885871104.fuzz");
+        CHECK(run_input(input.data(), input.size()) == 0);
+    }
+}