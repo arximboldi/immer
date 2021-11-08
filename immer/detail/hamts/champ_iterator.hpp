99741d7c73e5dbf03dfd0526c218327f72f6e780
Dwight Guth <dwight.guth@runtimeverification.com>
2021-11-08 15:06:15 -0600

    remove ignored qualifier

--- immer/detail/hamts/champ_iterator.hpp
+++ immer/detail/hamts/champ_iterator.hpp
@@ -88,7 +88,7 @@ struct champ_iterator
         std::copy(other.path_, other.path_ + depth_ + 1, path_);
     }
 
-    const auto relocation_info() const { return relocation_info_; }
+    auto relocation_info() const { return relocation_info_; }
 
 private:
     friend iterator_core_access;
