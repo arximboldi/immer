7f77e9bc85bc71d20db3351c4323ab1f40cfc176
Fabian Schiebel <fabian.schiebel@iem.fraunhofer.de>
2023-12-14 18:06:48 +0100

    minor reformatting

--- immer/detail/hamts/champ.hpp
+++ immer/detail/hamts/champ.hpp
@@ -149,8 +149,7 @@ struct champ
     champ(node_t* r, size_t sz = 0)
         : root{r}
         , size{sz}
-    {
-    }
+    {}
 
     champ(const champ& other)
         : champ{other.root, other.size}
@@ -1619,15 +1618,13 @@ struct champ
             , data{a.data}
             , owned{false}
             , mutated{false}
-        {
-        }
+        {}
         sub_result_mut(sub_result a, bool m)
             : kind{a.kind}
             , data{a.data}
             , owned{false}
             , mutated{m}
-        {
-        }
+        {}
         sub_result_mut()
             : kind{kind_t::nothing}
             , mutated{false} {};
