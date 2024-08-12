2af3b9db7b36b23f32e64721b0a6cb44b5933eca
Alex Shabalin <alex.shabalin@pm.me>
2024-08-12 15:27:35 +0200

    Simplify xx_hash definitions

--- immer/extra/persist/xxhash/xxhash.hpp
+++ immer/extra/persist/xxhash/xxhash.hpp
@@ -4,24 +4,21 @@
 
 namespace immer::persist {
 
-template <typename T>
-struct xx_hash;
-
-std::size_t xx_hash_value_string(const std::string& str);
-
-template <class T, class U>
-using enable_for = std::enable_if_t<std::is_same_v<T, U>, std::size_t>;
-
-template <class T>
-enable_for<T, std::string> xx_hash_value(const T& str)
-{
-    return xx_hash_value_string(str);
-}
-
 template <class T>
 struct xx_hash
 {
     std::size_t operator()(const T& val) const { return xx_hash_value(val); }
+};
+
+std::size_t xx_hash_value_string(const std::string& str);
+
+template <>
+struct xx_hash<std::string>
+{
+    std::size_t operator()(const std::string& val) const
+    {
+        return xx_hash_value_string(val);
+    }
 };
 
 } // namespace immer::persist
