cd44156f6176a9ad2dba1fb59fd4b3816cb1e946
jj683 <jj683@protonmail.com>
2024-10-24 11:28:42 +0200

    Add unsafe vector test case

--- test/vector/unsafe.cpp
+++ test/vector/unsafe.cpp
@@ -6,7 +6,6 @@
 // See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
 //
 
-#include <immer/flex_vector.hpp>
 #include <immer/memory_policy.hpp>
 #include <immer/vector.hpp>
 
@@ -15,11 +14,7 @@ using unsafe_memory = immer::memory_policy<immer::unsafe_free_list_heap_policy<i
                                            immer::no_lock_policy>;
 
 template <typename T>
-using test_flex_vector_t = immer::flex_vector<T, unsafe_memory, 3u, 6u>;
-
-template <typename T>
 using test_vector_t = immer::vector<T, unsafe_memory, 3u, 6u>;
 
-#define FLEX_VECTOR_T test_flex_vector_t
 #define VECTOR_T test_vector_t
 #include "generic.ipp"
